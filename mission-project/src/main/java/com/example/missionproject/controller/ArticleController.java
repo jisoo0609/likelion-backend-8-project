08df2d07b7f0a13d8c302db9224e9be8942048f5
jisoo0609 <jisu20000609@gmail.com>
2024-01-08 18:18:58 +0900

    게시글 수정 기능 추가

---
+++ mission-project/src/main/java/com/example/missionproject/controller/ArticleController.java
@@ -0,0 +1,75 @@
+package com.example.missionproject.controller;
+
+
+import com.example.missionproject.entity.Article;
+import com.example.missionproject.repository.ArticleRepository;
+import com.example.missionproject.service.ArticleService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("article")
+public class ArticleController {
+    private final ArticleService articleService;
+    private final ArticleRepository articleRepository;
+
+    // 게시글 상세보기
+    @GetMapping("{articleId}")
+    public String readArticle(@PathVariable("articleId") Long id, Model model) {
+        Article article = articleService.readArticle(id);
+        model.addAttribute("article", article);
+        model.addAttribute("boardId", article.getBoard().getId());
+        return "articles/article-view";
+    }
+
+    // 게시글 수정 페이지로 이동
+    @GetMapping("{articleId}/update")
+    public String updateView(@PathVariable("articleId")Long id, Model model) {
+        Article article = articleService.readArticle(id);
+        model.addAttribute("article", article);
+        return "articles/update";
+    }
+
+    @PostMapping("{articleId}")
+    public String updateArticle(
+            @PathVariable("articleId") Long id,
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("author") String author,
+            @RequestParam("password") String password) {
+        // 비밀번호가 일치하는 경우
+        if (articleService.isPasswordCorrect(id, password)) {
+            articleService.update(id, title, content, author, password);
+            return String.format("redirect:/article/%d", id);
+        } else {
+            // 비밀번호 일치하지 않는 경우
+            return String.format("redirect:/article/%d/update?error=true", id);
+        }
+    }
+
+    // 게시글 삭제하기
+    @GetMapping("{articleId}/delete")
+    public String deleteView(@PathVariable("articleId") Long id, Model model) {
+        Article article = articleService.readArticle(id);
+        model.addAttribute("article", article);
+        model.addAttribute("boardId", article.getBoard().getId());
+        return "articles/delete";
+    }
+
+    @PostMapping("{articleId}/delete")
+    public String deleteArticle(@PathVariable("articleId") Long id,
+                                @RequestParam String password,
+                                @RequestParam Long boardId) {
+        if (articleService.isPasswordCorrect(id, password)) {
+            articleService.delete(id);
+            return String.format("redirect:/boards/%d", boardId);
+        } else {
+            return String.format("redirect:/article/%d/delete?error=true", id);
+        }
+    }
+}
---
+++ mission-project/src/main/java/com/example/missionproject/service/ArticleService.java
@@ -0,0 +1,50 @@
+package com.example.missionproject.service;
+
+import com.example.missionproject.entity.Article;
+import com.example.missionproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    // 게시글 클릭하면 상세 게시글 보여줌
+    public Article readArticle(Long id) {
+        Optional <Article> optionalArticle = articleRepository.findById(id);
+        return optionalArticle.orElse(null);
+    }
+
+    // 게시글 수정하기 위해 비밀번호를 확인하는 메서드
+    public boolean isPasswordCorrect(Long id, String password) {
+        // 게시글을 가져옴
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        // 가져온 게시글의 password와 입력받은 password가 일치하는지 확인
+        if (optionalArticle.isPresent()) {
+            Article article = optionalArticle.get();
+            return article.getPassword().equals(password);
+        }
+        return false;
+    }
+
+    // 업데이트
+    public void update(Long id, String title, String content, String author, String password) {
+        // 비밀번호가 일치하는 경우 대상 데이터 찾음
+        Article target = readArticle(id);
+        // 데이터의 내용을 전달받은 내용으로 갱신
+        target.setTitle(title);
+        target.setContent(content);
+        target.setAuthor(author);
+        target.setPassword(password);
+        // save
+        articleRepository.save(target);
+    }
+
+    // 게시글 삭제하기
+    public void delete(Long id) {
+        articleRepository.deleteById(id);
+    }
+}
